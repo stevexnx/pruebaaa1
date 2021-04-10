59facec12e89c0902ca08a9a0544d32c8398ec21
stevexnx <39393462+stevexnx@users.noreply.github.com>
2021-04-10 14:31:31 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import telepot
 import os
 import time
 
+
 global reg
 reg=[]
 
@@ -10,16 +11,17 @@ def handle(msg):
     #registros=[]
     chat_id = msg['chat']['id']
     command = msg['text']
+
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-    def start():
+    def start(command):
         try:
-            bot.sendMessage(chat_id,("Bot de Parqueo"))
-            bot.sendMessage(chat_id,("Puede realizar lo siguiente: \n"))
-            bot.sendMessage(chat_id,("1) /ingresar (placa carro)\nIngresa carro al parqueo"))
-            bot.sendMessage(chat_id,("2) /consultar (placa carrp)\nConsultar si el carro ha entrado al parqueo"))
-            bot.sendMessage(chat_id,("3) /borrar (placa)\nCarro sale del parqueo"))
+            bot.sendMessage(chat_id,("Bienvenido"))
+            bot.sendMessage(chat_id,("Puedes utilizar los siguientes comandos: \n"))
+            bot.sendMessage(chat_id,("1) /ingresar + (tu matrícula). Para agregarte a la cola."))
+            bot.sendMessage(chat_id,("2) /consultar + (tu matrícula). Para consultar tu numero de ticket."))
+            bot.sendMessage(chat_id,("3) /borrar + (tu matrícula). Para eliminarte de la lista."))
         except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #S1. Si persiste, comuniquese con el administrador.")
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
     def Ingresar_Articulo(command):
         try:
@@ -27,13 +29,13 @@ def handle(msg):
             placa=int(datos[1])
             if len(reg)<10:
                 reg.append(placa)
-                bot.sendMessage(chat_id, "Aun hay parqueo disponible")
-                bot.sendMessage(chat_id, ("Entro el Carro"))
+                bot.sendMessage(chat_id, "Aun hay tickets disponibles")
+                bot.sendMessage(chat_id, ("Usted a sido añadido satisfactoriamente."))
                 bot.sendMessage(chat_id, reg)
             else:
-                bot.sendMessage(chat_id ,"ya no hay parqueo")
+                bot.sendMessage(chat_id ,"No quedan asientos.")
         except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #I1. Si persiste, comuniquese con el administrador.")
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
     def Consultar_Articulo(command):
@@ -41,11 +43,11 @@ def handle(msg):
             comp=command.split()
             c=int(comp[1])
             if c in reg:
-                bot.sendMessage(chat_id,"El carro si esta en el parqueo")
+                bot.sendMessage(chat_id,"Usted ya esta registrado.")
             else:
-                bot.sendMessage(chat_id, "El carro no esta en el parqueo\nLe puso llave :D")
+                bot.sendMessage(chat_id, "Usted aún no esta registrado.")
         except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #C1. Si persiste, comuniquese con el administrador.")
             
     #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
     def Borrar(command):#funcion para borrar un articulo
@@ -54,13 +56,13 @@ def handle(msg):
             c=int(comp[1])
             if c in reg:
                 reg.remove(c)
-                bot.sendMessage(chat_id,"El carro ha salido del parqueo")
+                bot.sendMessage(chat_id,"Usted ha sido eliminado satisfactoriamente.")
             else:
-                bot.sendMessage(chat_id, "El carro no esta en el parqueo\nLe puso llave :D")
+                bot.sendMessage(chat_id, "Usted no esta registrado.")
         except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
+            bot.sendMessage(chat_id,"Ha habido algún error. \nCodigo #B1. Si persiste, comuniquese con el administrador.")
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-    lista1=['/ingresar','/consultar','/borrar']
+    lista1=['start','/ingresar','/consultar','/borrar']
     div=command.split()
     comparacion = []
     for item in lista1:
@@ -68,16 +70,16 @@ def handle(msg):
             comparacion.append(item)
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
     try:
-        if command==("/start"):#comparo y decido que funcion se ejecutara, para el caso de /start, como no contiene valores de entrada de usuario va directo
-            start()#la funcion tiene los valores de entrada del id de la persona y el objeto bot, para no utilizaro 2 veces
-        elif comparacion[0]==("/ingresar"):#a esta funcion le envio command porque las cosas llegan por ejemplo
-            Ingresar_Articulo(command)#/Ingresar 123(codigo) 25(cantidad) 100(precio) zapatos(nombre)
-        elif comparacion[0]==("/consultar"):#para diferenciar eso hago lo del .split()
-            Consultar_Articulo(command)#haci con todas las funciones similares
-        elif comparacion[0]==("/borrar"):
-            Borrar(command)
+        if command==("/start"):#Para inicar el bot#
+            start(command)#la funcion tiene los valores de entrada del id de la persona y el objeto bot, para no utilizaro 2 veces#
+        elif comparacion[0]==("/ingresar"):#Para comprar un ticket#
+            Ingresar_Articulo(command)#Solo es posible infresar caracteres numericos#
+        elif comparacion[0]==("/consultar"):#Para consultar si has comprado un ticket#
+            Consultar_Articulo(command)#Se debe ingresar la matricula.
+        elif comparacion[0]==("/borrar"):#Para borrar tu compra#
+            Borrar(command)#Se debe ingresar la matricula que se quiere borrar#
     except(IndexError):
-        bot.sendMessage(chat_id, ("Ingreso una funcion no valida"))
+        bot.sendMessage(chat_id, ("Ha habido algún error. \nCodigo. #CM.Error. Si persiste, comuniquese con el administrador."))
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 
 
